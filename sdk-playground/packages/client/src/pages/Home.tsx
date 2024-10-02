13b1d5c269070da84097fd5f3cef754e545c4815
Andrew Giel <andrew.giel@discordapp.com>
2024-10-02 11:14:12 -0400

    chore(sdk-playground): add basic info to Home (#19)
    * chore(sdk-playground): add basic info to Home
    
    * update pnpm-lock.yaml

--- sdk-playground/packages/client/src/pages/Home.tsx
+++ sdk-playground/packages/client/src/pages/Home.tsx
@@ -1,4 +1,13 @@
+import discordSdk from '../discordSdk';
+
 export default function Home() {
+  const instanceId = discordSdk.instanceId;
+  const clientId = discordSdk.clientId;
+  const channelId = discordSdk.channelId;
+  const guildId = discordSdk.guildId;
+  const platform = discordSdk.platform;
+  const mobileAppVersion = discordSdk.mobileAppVersion;
+  const sdkVersion = discordSdk.sdkVersion;
   return (
     <div style={{padding: 32}}>
       <h1>Welcome to the SDK playground! 🎉</h1>
@@ -6,6 +15,15 @@ export default function Home() {
         This is a place for testing out all of the capabilities of the Embedded App SDK as well as validating
         platform-specific behavior related to Activities in Discord.
       </p>
+      <br></br>
+      <h3> Basic Activity Info</h3>
+      <p> Instance ID: {instanceId} </p>
+      <p> Client ID: {clientId} </p>
+      <p> Channel ID: {channelId} </p>
+      { guildId != null ? (<p> Guild ID: {guildId} </p>) : null }
+      <p> Platform: {platform} </p>
+      { mobileAppVersion != null ? (<p> Mobile App Version: {mobileAppVersion} </p>) : null }
+      <p> Using SDK Version: {sdkVersion } </p>
     </div>
   );
 }
