37bf578e97c3453bf5eaafd18f673bd026b43798
Yoni Lerner <yoni.lerner@discordapp.com>
2024-11-27 14:00:49 -0800

    tabs -> spaces

--- sdk-playground/packages/client/vite.config.ts
+++ sdk-playground/packages/client/vite.config.ts
@@ -1,26 +1,26 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, '../../', '');
-	return {
-		plugins: [react()],
-		envDir: '../../',
-		server: {
-			port: Number.parseInt(env.WEBAPP_SERVE_PORT),
-			proxy: {
-				'/api': {
-					target: 'http://localhost:8787',
-					changeOrigin: true,
-					secure: false,
-					ws: true,
-					// rewrite: (path) => path.replace(/^\/api/, ""),
-				},
-			},
-			hmr: {
-				clientPort: 443,
-			},
-		},
-	};
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, '../../', '');
+    return {
+        plugins: [react()],
+        envDir: '../../',
+        server: {
+            port: Number.parseInt(env.WEBAPP_SERVE_PORT),
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:8787',
+                    changeOrigin: true,
+                    secure: false,
+                    ws: true,
+                    // rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
+            hmr: {
+                clientPort: 443,
+            },
+        },
+    };
 });
--- sdk-playground/packages/server/src/handlers/getActivityInstanceHandler.ts
+++ sdk-playground/packages/server/src/handlers/getActivityInstanceHandler.ts
@@ -1,21 +1,21 @@
-import type { Env, IGetOAuthToken } from '../types';
-import { readRequestBody, requestHeaders } from '../utils';
+import type {Env, IGetOAuthToken} from '../types';
+import {readRequestBody, requestHeaders} from '../utils';
 
 export default async function getActivityInstanceHandler(
-	path: string[],
-	request: Request,
-	env: Env,
+    path: string[],
+    request: Request,
+    env: Env,
 ) {
-	try {
-		const instanceId = path[1];
-		return await fetch(
-			`${env.VITE_DISCORD_API_BASE}/applications/${env.VITE_CLIENT_ID}/activity-instances/${instanceId}`,
-			{
-				headers: requestHeaders(env),
-			},
-		);
-	} catch (ex) {
-		console.error(ex);
-		return new Response(`Internal Error: ${ex}`, { status: 500 });
-	}
+    try {
+        const instanceId = path[1];
+        return await fetch(
+            `${env.VITE_DISCORD_API_BASE}/applications/${env.VITE_CLIENT_ID}/activity-instances/${instanceId}`,
+            {
+                headers: requestHeaders(env),
+            },
+        );
+    } catch (ex) {
+        console.error(ex);
+        return new Response(`Internal Error: ${ex}`, {status: 500});
+    }
 }
