305c4774e917785e34450aa609ec0d04c8fef967
Andrew Giel <andrew.giel@discordapp.com>
2025-01-08 11:38:21 -0500

    add referrerId

--- sdk-playground/packages/client/src/pages/ShareLink.tsx
+++ sdk-playground/packages/client/src/pages/ShareLink.tsx
@@ -4,7 +4,7 @@ import discordSdk from '../discordSdk';
 export default function ShareLink() {
   const [message, setMessage] = React.useState<string>('Come Play SDK Playground!');
   const [customId, setCustomId] = React.useState<string | undefined >(undefined);
-
+  const [referrerId, setReferrerId] = React.useState<string | undefined >(undefined);
 
   const [hasPressedSend, setHasPressedSend] = React.useState<boolean>(false);
   const [didSend, setDidSend] = React.useState<boolean>(false);
@@ -15,11 +15,15 @@ export default function ShareLink() {
   const handleCustomIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCustomId(event.target.value);
   };
+  const handleReferrerIdChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setReferrerId(event.target.value);
+  };
 
   const doShareLink = async () => {
     const { success} = await discordSdk.commands.shareLink({
       message,
       custom_id: customId,
+      referrer_id: referrerId,
     });
     setHasPressedSend(true);
     setDidSend(success);
@@ -27,6 +31,7 @@ export default function ShareLink() {
 
   return (
     <div style={{padding: 32}}>
+      <p> Message: </p>
       <input
         type="text"
         value={message}
@@ -35,6 +40,7 @@ export default function ShareLink() {
         style={{ width: '400px', padding: '8px' }}
       />
       <br/>
+      <p> Custom ID: </p>
       <input
         type="text"
         value={customId}
@@ -43,6 +49,16 @@ export default function ShareLink() {
         style={{ width: '400px', padding: '8px' }}
       />
       <br/>
+      <p> Referrer ID: </p>
+      <input
+        type="text"
+        value={referrerId}
+        onChange={handleReferrerIdChange}
+        placeholder="What's your referrer ID?"
+        style={{ width: '400px', padding: '8px' }}
+      />
+      <br/>
+
       <button onClick={doShareLink}>Click to Share this Link!</button>
       { hasPressedSend ? (didSend ? (<p> Succesfully shared! </p>) : (<p> Did not share </p>)) : null }
     </div>
