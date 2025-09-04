2d24d0456ed5abef6e3984837f7101b2a4b51dd4
Andrew Giel <andrew.giel@discordapp.com>
2025-09-04 16:16:30 -0400

    remove dist oops

--- sdk-playground/packages/shared/dist/crypto.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Env, ProxyToken } from './types';
-export declare function getSubtleCrypto(): SubtleCrypto;
-export declare function verifyProxyAuthHeaders(signatureHeader: string | null, timestampHeader: string | null, payloadHeader: string | null, publicKey: Uint8Array): Promise<ProxyToken | null>;
-export declare function verifyProxyAuth(request: Request, env: Env): Promise<ProxyToken | null>;
-export declare function withProxyAuth(handler: (proxyToken: ProxyToken, path: string[], request: Request, env: Env) => Promise<Response> | Response, request: Request, env: Env, path: string[]): Promise<Response>;
